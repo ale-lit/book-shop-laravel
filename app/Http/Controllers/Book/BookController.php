7d4817e9cc453d12509ec9134755246215c02458
ale-lit <zeldor@ya.ru>
2022-06-10 04:07:09 +0300

    Update BookController.php

--- app/Http/Controllers/Book/BookController.php
+++ app/Http/Controllers/Book/BookController.php
@@ -54,6 +54,7 @@ class BookController extends BaseController
         //     'book_name' => 'required|min:5|max:255'
         // ];
         // $validatedData = $this->validate($request, $rules);
+
         $data = $request->all();
         $item = new Book();
         $item->book_is_deleted = 0;
